d3a6812a008f8135728425a0ed0bfda8ae9a65fe
Mehtap Uğur <yaziliminval@hotmail.com>
2021-09-12 20:10:53 +0300

    Add credit task

---
+++ Week 01/credit.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+
+    long number = get_long("Enter your Credit Card Number: \n");
+    long check = number;
+
+    int counter = 0;
+    int ilk_iki;
+
+    while(check > 0){
+        if(check < 100 && check > 10){ // ilk iki basamağa ihtiyacımız var
+            ilk_iki = check;
+        }
+    
+        counter++; //basamak sayısı
+        check /= 10; //her seferinde 10a bölüp basamak azaltılır
+    }
+
+    if((counter != 13) && (counter != 15) && (counter != 16)){ //bu basamak sayısına sahip değilse kontrol edilmez
+        printf("INVALID\n");
+
+    } else {
+        int a = 0; //altı çizili olmayan sayıları toplama sayacı
+        int b = 0; //altı çizili olan sayıları toplama sayacı
+        int c = 0;
+
+        while( number > 0){ //check yukarıda değiştiği için number kullanıldı
+            a += number%10; // 45623879564542
+            number /= 10; // 4562387956454.2
+
+            if(number > 0){
+                c = (number%10) *2; // tekrar mod alır sondan 2. basamağı bulur, 2 ile çarpar
+                b += c/10; // sonucun ilk basamağı, sonuç 17 ise 1 i b ye ekler
+                b += c%10; // sonucun 2. basamağı, sonuç 17 ise 7 yi b ye ekler
+                number /= 10;
+            }
+        }
+
+        int rmn = (a+b)%10; // kontrol için toplamın son basamağı elde edilir
+        
+        if(rmn != 0){
+            printf("INVALID\n");
+
+        } else if(rmn == 0){
+
+            if((counter == 15) && ((ilk_iki == 34) || (ilk_iki == 37))){
+                printf("AMEX\n");
+
+            }
+            else if((counter == 16) && ((ilk_iki == 51) || (ilk_iki == 52) || (ilk_iki == 53) ||
+            (ilk_iki == 54) || (ilk_iki == 55) )){
+
+                     printf("MASTERCARD\n");
+            }
+            else if(((counter == 13) || (counter = 16)) && (ilk_iki / 10)==4 ){
+                printf("VISA\n");
+
+            }
+            else {
+                printf("INVALID\n");
+            }
+        }
+    }
+}
